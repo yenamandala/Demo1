7c8000425d69ac7d82ed689ed42c1642d7eaf2b9
yenamandala <93111273+yenamandala@users.noreply.github.com>
2024-01-16 13:30:07 +0530

    Add files via upload
    toy simulator

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp2;
+
+class Program
+{
+    static void Main()
+    {
+        Toysimulator robot = new Toysimulator();
+
+        robot.Place(0, 0, Direction.NORTH);
+        robot.Move();
+        robot.Report(); 
+
+        robot.Place(0, 0, Direction.NORTH);
+        robot.Left();
+        robot.Report();
+    }
+}
---
+++ Toysimulator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    
+    public class Toysimulator
+    {
+        private int x;
+        private int y;
+        private Direction facing;
+
+     
+        public void Place(int x, int y, Direction facing)
+        {
+            this.x = x;
+            this.y = y;
+            this.facing = facing;
+        }
+
+        public void Move()
+        {
+            switch (facing)
+            {
+                case Direction.NORTH:
+                    if (y < 4) y++;
+                    break;
+                case Direction.SOUTH:
+                    if (y > 0) y--;
+                    break;
+                case Direction.EAST:
+                    if (x < 4) x++;
+                    break;
+                case Direction.WEST:
+                    if (x > 0) x--;
+                    break;
+            }
+        }
+
+        public void Left()
+        {
+            facing = (Direction)(((int)facing + 3) % 4);
+        }
+
+        public void Right()
+        {
+            facing = (Direction)(((int)facing + 1) % 4);
+        }
+
+        public void Report()
+        {
+            Console.WriteLine($"{x},{y},{facing}");
+        }
+    }
+
+    public enum Direction
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+
+}
